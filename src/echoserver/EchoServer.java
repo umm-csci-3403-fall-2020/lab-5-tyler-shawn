02e98b7d25924de567c1b48bdc2253fedb972528
tyler <rowla070@morris.umn.edu>
2020-10-13 17:38:27 -0500

    EchoServer is complete, passes bats tests. I was using Buffer read and PrintWriter which doesn't handle byte oriented things. Used .read() and .write() instead

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -11,21 +11,26 @@ public class EchoServer {
             // Start listening on the specified port
             ServerSocket sock = new ServerSocket(portNumber);
 
+
             // Run forever, which is common for server style services
             while (true) {
                 // Wait until someone connects, thereby requesting a date
                 Socket client = sock.accept();
                 System.out.println("Got a request!");
 
+                InputStream input = client.getInputStream();
+                OutputStream output = client.getOutputStream();
+
                 // Construct a writer so we can write to the socket, thereby
                 // sending something back to the client.
-                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
-                BufferedReader read = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-                // Send the current date back tothe client.
-                boolean line;
-                while(line = read.readLine() != null){
-                    writer.println(line);
+
+                // Send the current date back to the client.
+                byte[] bytes = new byte[16*1024];
+
+                int line;
+                while ((line = input.read(bytes)) > 0) {
+                    output.write(bytes, 0, line);
                 }
                 // Close the client socket since we're done.
                 client.close();
